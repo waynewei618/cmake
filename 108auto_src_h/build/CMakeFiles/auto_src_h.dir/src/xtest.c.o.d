51017089175da0f74502c4fecf44f1b4a7e82d1f
wayne.wei618 <wayne.wei618@gmail.com>
2023-12-30 20:52:26 +0800

    delete

--- 108auto_src_h/build/CMakeFiles/auto_src_h.dir/src/xtest.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/auto_src_h.dir/src/xtest.c.o: \
- /mnt/d/worksapce/cmake/108auto_src_h/src/xtest.c \
- /usr/include/stdc-predef.h
--- 109cmake_out/build/CMakeFiles/xlog.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for xlog.
-# This may be replaced when dependencies are built.
